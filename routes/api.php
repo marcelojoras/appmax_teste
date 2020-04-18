540f4d01f57234a83babde62b7740da71d7d9132
Marcelo Joras <marcelojoras@hotmail.com>
2020-04-18 14:28:47 -0300

    Inserindo API's
    Inserindo as API's de criar e deletar produtos.

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,6 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/product/create', 'MainController@store');
+Route::delete('/product/delete/{id}', 'MainController@destory');
